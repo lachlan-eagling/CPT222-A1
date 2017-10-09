02f6f2488d8899c64c76baa70f76367c44734547
Lachlan Eagling <lochie.eagling@gmail.com>
2017-10-09 20:14:58 +1100

    Add MainWindowController

---
+++ src/au/edu/rmit/cpt222/model/controller/MainWindowController.java
@@ -0,0 +1,19 @@
+package au.edu.rmit.cpt222.model.controller;
+
+import sun.applet.Main;
+
+public class MainWindowController {
+
+    public MainWindowController(){
+
+    }
+
+    // TODO: Implement newGame event handler.
+    // TODO: Implement addPlayer event handler.
+    // TODO: Implement editPlayer event handler.
+    // TODO: Implement spinCoing eventHandler.
+    // TODO: Implement method to update coin label.
+    // TODO: Implement method to update player details.
+    // TODO: Implement method to update game outcome.
+
+}
